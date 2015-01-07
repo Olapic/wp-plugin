871df45bdafcbff5fa82b5644a153c84c196182a
Robert Pisano <robertpisano@yahoo.com>
2015-01-07 17:21:37 -0500

    added dev-mode support

--- WordpressPluginNewWidgets.php
+++ WordpressPluginNewWidgets.php
@@ -11,7 +11,7 @@ Author URI: http://www.olapic.com/
 /*
 Simple Olapic widget for #brooksrunning (Wordpress Plugin)
 
-USAGE: [olapic data_tags ='' data_instance = '' data_apikey='']
+USAGE: [olapic data_tags ='' data_instance ='' data_apikey='' data_mode'']
 */
 
 //tell wordpress to register the demolistposts shortcode
@@ -28,7 +28,8 @@ function olapic_function($atts) {
   				array(
   					'data_instance' => '',
             'data_apikey' => '',
-            'data_tags' => ''
+            'data_tags' => '',
+            'data_mode' => ''
   				),
   				$atts
   			);
@@ -36,11 +37,16 @@ function olapic_function($atts) {
   $data_instance= 'data-instance="'.$arguments['data_instance'].'" ';
   $data_apikey= 'data-apikey="'.$arguments['data_apikey'].'" ';
   $data_tags = '';
+  $data_mode = '';
   if($arguments['data_tags'] != '')
  		$data_tags= 'data-tags="'.$arguments['data_tags'].'" ';
+  if($arguments['data_mode'] != '')
+    $data_mode='data-mode="'.$arguments['data_mode'].'" ';
 
 
-  $olapic_output = '<div id="olapic_specific_widget"></div><script type="text/javascript" src="//photorankstatics-a.akamaihd.net/81b03e40475846d5883661ff57b34ece/static/frontend/latest/build.min.js"  data-olapic="olapic_specific_widget" '.$data_instance.$data_apikey.$data_tags.'async="async"></script>';
+
+
+  $olapic_output = '<div id="olapic_specific_widget"></div><script type="text/javascript" src="//photorankstatics-a.akamaihd.net/81b03e40475846d5883661ff57b34ece/static/frontend/latest/build.min.js"  data-olapic="olapic_specific_widget" '.$data_instance.$data_apikey.$data_tags.$data_mode.'async="async"></script>';
   //send back text to calling function
   return $olapic_output;
 }
