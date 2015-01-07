18bcfd9568a9db52799ad95ab329fc34363f0b0e
Robert Pisano <robertpisano@yahoo.com>
2015-01-07 17:07:19 -0500

    plugin

---
+++ WordpressPluginNewWidgets.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin Name: Olapic Brooks Running Widget
+Plugin URI: http://www.olapic.com/
+Description: Simple plugin for #brooksrunning
+Version: 1.0
+Author: Jae Han Kim (Olapic)
+Author URI: http://www.olapic.com/
+*/
+
+/*
+Simple Olapic widget for #brooksrunning (Wordpress Plugin)
+
+USAGE: [olapic data_tags ='' data_instance = '' data_apikey='']
+*/
+
+//tell wordpress to register the demolistposts shortcode
+add_shortcode("olapic", "olapic_handler");
+
+function olapic_handler($atts) {
+  $olapic_output = olapic_brooksrunning_function($atts); 
+  return $olapic_output;
+}
+
+function olapic_function($atts) {
+  //process plugin
+  $arguments = shortcode_atts(
+  				array(
+  					'data_instance' => '',
+            'data_apikey' => '',
+            'data_tags' => ''
+  				),
+  				$atts
+  			);
+
+  $data_instance= 'data-instance="'.$arguments['data_instance'].'" ';
+  $data_apikey= 'data-apikey="'.$arguments['data_apikey'].'" ';
+  $data_tags = '';
+  if($arguments['data_tags'] != '')
+ 		$data_tags= 'data-tags="'.$arguments['data_tags'].'" ';
+
+
+  $olapic_output = '<div id="olapic_specific_widget"></div><script type="text/javascript" src="//photorankstatics-a.akamaihd.net/81b03e40475846d5883661ff57b34ece/static/frontend/latest/build.min.js"  data-olapic="olapic_specific_widget" '.$data_instance.$data_apikey.$data_tags.'async="async"></script>';
+  //send back text to calling function
+  return $olapic_output;
+}
+?>
