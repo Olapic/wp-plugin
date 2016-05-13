0e94cc9d2528487649f795b5a9f420e48b74835e
Jae Kim <jae@olapic.com>
2016-05-13 11:04:58 -0400

    repackage

--- README.md
+++ README.md
@@ -1,35 +1,37 @@
-# wp-plugin
+# Olapic wp-plugin
 
-Olapic WordPress Plugin. Wordpress doesn't like external JS being injected in post content, so this plugin allows for Olapic code injection via shortcode.
+Olapic WordPress Plugin. 
+
+Wordpress doesn't like external JS being injected in post content, so this plugin allows for Olapic code injection via shortcode.
 
 # Usage
 
-Using this plugin requires 2 step process:
+Using this plugin requires a simple two-step process:
 
 1. Install the plugin
-2. Use the shortcode (e.g., `[olapic data_instance ="ABC" data_apikey="XYZ" data_tags="PID"]`
+	- **Option A:** Unzip the .zip file into the  WordPress Plugin Directory
+	- **Option B:** Upload the .zip file via the plugin upload page (`http://yourdomain.com/wp-admin/plugin-install.php?tab=upload`)
 
-# Parameters
+2. Activate the plugin
+	![](https://dl.dropboxusercontent.com/s/i2ppn1mew4ob5wu/2016-05-13%20at%2010.15%20AM%20%281%29.png)
 
-The shortcode takes in 3 params:
+3. In the Wordpress post, use the shortcode: 
 
-* `data_instance` - **required**
+	`[olapic instance-id ="WIDGET_INSTANCE_ID" apikey="OLAPIC_API_KEY" tags="PRODUCT_ID"]`. 
+
+	See below for shortcode parameter details.
+
+# Shortcode Parameters
+
+The shortcode takes in 4 parameters:
+
+* `instance-id` - **required**
 	* You can find this value from the `data-instance` part of the original widget code.
-* `data_apikey` - **required**
-	* You can find this value from the `data-apikey` part of the original widget code.
-* `data_tags` - *optional*
-	* This serves as the dynamic identifier that you wish to get all related UGC for. The value usually is the product identifier from your product feed which we ingest during integration.
-* `data_mode` - *optional*
-	* Accepted value: `development`
+* `apikey` - **required**
+	* You can find this value from the `data-apikey` part of the original widget code. ()
+* `tags` - *optional*
+	* This serves as the dynamic identifier to pull content from a specific stream. Often, the value is the product identifier from your product feed.
+* `mode` - *optional*
+	* Accepted parameter value: `development`
 	* You can use this flag to serve all content from the sandbox environment.
-	* Note that this parameter **SHOULD NOT** be utilized for production purposes.
-
-
-## Installing the plugin.
-Either:
-
-* Copy the `olapic-widget` folder from this repo to your `wp-content/plugins` folder
-
-OR.
-
-* Upload the `wp-olapic-widget.tar.gz` from this repo using the "Plugins" section of WordPress.
+	* Note that this parameter ***should not*** be utilized for production purposes.
--- olapic-widget/olapic-widget.php
+++ olapic-widget/olapic-widget.php
@@ -2,42 +2,34 @@
 /*
 Plugin Name: Olapic Shortcode Widget
 Plugin URI: http://www.olapic.com/
-Description: Plugin to utilize shortcodes in order to surface olapic codes on wordpress
-Version: 1.0
-Author: Robert Pisano(Olapic)
-Co-Author: Jae Han Kim (Olapic)
+Description: Plugin to utilize shortcodes in order to surface Olapic Javascript codes on WordPress Posts
+Version: 1.1
+Author: Olapic
+Co-Author: Robert Pisano (Olapic), Jae Han Kim (Olapic)
 Author URI: http://www.olapic.com/
-USAGE: in WP template - [olapic instance ='abc123' apikey='123abc' mode='development' tags ='(if applicaple insert here)']
+USAGE: In WP post, use the following shortcode: [olapic instance ='abc123' apikey='123abc' mode='development' tags ='(if applicaple insert here)']
 */
 
-/*
-Olapic Plugin to utilize shortcode. It's made general to be more robust.
-*/
+// Tell wordpress to register the "olapic" shortcode
+add_shortcode('olapic', 'olapic_function');
 
-//tell wordpress to register the demolistposts shortcode
-add_shortcode("olapic", "olapic_handler");
+function olapic_function($atts)
+{
+    $arguments = shortcode_atts(array(
+        'instance-id' => '',
+        'apikey' => '',
+        'tags' => '',
+        'mode' => '',
+    ), $atts);
 
-function olapic_handler($atts) {
-  $olapic_output = olapic_function($atts); 
-  return $olapic_output;
+    $data_instance = "data-instance=\"{$arguments['instance-id']}\"";
+    $data_apikey = "data-apikey=\"{$arguments['apikey']}\"";
+    $data_tags = !empty($arguments['tags']) ? "data-tags=\"{$arguments['tags']}\"" : '';
+    $data_mode = !empty($arguments['mode']) ? "data-mode=\"{$arguments['mode']}\"" : '';
+    //since data-tags and data-mode aren't required, checks if its empty before giving it a value
+
+    $olapic_output = "<div id=\"olapic_specific_widget\"></div><script type=\"text/javascript\" src=\"//photorankstatics-a.akamaihd.net/81b03e40475846d5883661ff57b34ece/static/frontend/latest/build.min.js\"  data-olapic=\"olapic_specific_widget\" {$data_instance} {$data_apikey} {$data_tags} {$data_mode} async=\"async\"></script>";
+    //changed from using concatenation. Concatenation using the dot operator will concatenate each one individually. Using curly braces will place the variables in once - better practice when using multiple concatenations
+
+    return $olapic_output;
 }
-
-function olapic_function($atts) {
-  //process plugin
-  $arguments = shortcode_atts(array('instance' => '', 'apikey' => '', 'tags' => '', 'mode' => ''), $atts);
-
-  $data_instance= "data-instance=\"{$arguments['instance']}\" " ;
-  $data_apikey= "data-apikey=\"{$arguments['apikey']}\" ";
-  $data_tags =  !empty($arguments['tags']) ? "data-tags=\"{$arguments['tags']}\" " : '';  
-  $data_mode = !empty($arguments['mode']) ? "data-mode=\"{$arguments['mode']}\" " : '';
-   //since data_tags and data_mode aren't required, checks if its empty before giving it a value
-
-
-
-
-
-  $olapic_output = "<div id=\"olapic_specific_widget\"></div><script type=\"text/javascript\" src=\"//photorankstatics-a.akamaihd.net/81b03e40475846d5883661ff57b34ece/static/frontend/latest/build.min.js\"  data-olapic=\"olapic_specific_widget\" {$data_instance}{$data_apikey}{$data_tags}{$data_mode}async=\"async\"></script>";
-  //changed from using concatenation. Concatenation using the dot operator will concatenate each one individually. Using curly braces will place the variables in once - better practice when using multiple concatenations
-  return $olapic_output;
-}
-?>
